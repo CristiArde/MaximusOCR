ff6e665d0b15d4ff4789f147048ba04f9add3d4f
cardel6 <auditek1>
2018-08-18 14:27:22 -0400

    added speech input functionality and intents

--- app/src/main/java/ai/bonobo/maximusocr/MaximusOCR.java
+++ app/src/main/java/ai/bonobo/maximusocr/MaximusOCR.java
@@ -1,22 +1,27 @@
 package ai.bonobo.maximusocr;
 
-import ai.bonobo.maximusocr.R;
+
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.speech.RecognizerIntent;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MaximusOCR extends AppCompatActivity {
 
     EditText totalTxt;
     ListView listView;
     ImageButton recordBtn;
-
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,5 +31,48 @@ public class MaximusOCR extends AppCompatActivity {
         totalTxt = (EditText)findViewById(R.id.totalTxt);
         listView = (ListView)findViewById(R.id.listView);
         recordBtn = (ImageButton)findViewById(R.id.recordBtn);
+
+        recordBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+                    public void onClick(View v){
+
+            }
+        });
+    }
+
+    private void startSpeechListener(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    totalTxt.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
     }
 }
